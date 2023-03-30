f8c363b90430c4574c320b233ba225126162b91d
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-03-30 20:31:56 -0300

    feat(camera): implemented the camera in the add component

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
 import { fetchUser } from '../redux/actions';
 
 import Feed from './Main/Feed';
@@ -77,11 +76,11 @@ const Main = props => {
   );
 };
 
-// const mapStateToProps = store => ({
-//   currentUser: store.userState.currentUser,
-// });
+const mapStateToProps = store => ({
+  currentUser: store.userState.currentUser,
+});
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ fetchUser }, dispatch);
 
-export default connect(null, mapDispatchToProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- components/Main/Add.js
+++ components/Main/Add.js
@@ -1,36 +1,32 @@
+import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Camera, CameraType } from 'expo-camera';
 
 const App = () => {
-    const [type, setType] = useState(CameraType.back);
-    const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-    // if (!permission || !permission.granted) {
-    //   return <Text>No access to camera</Text>;
-    // }
+  if (!permission || !permission.granted) {
+    return <Text>No access to camera</Text>;
+  }
 
-    const toggleCameraType = () => {
-      setType(current =>
-        current === CameraType.back ? CameraType.front : CameraType.back
-      );
-    };
-
-    return (
-      <View>
-        <Camera
-          type={type}
-        >
-          <View>
-            <TouchableOpacity
-              onPress={toggleCameraType}
-            >
-              <Text>Flip Camera</Text>
-            </TouchableOpacity>
-          </View>
-        </Camera>
-      </View>
+  const toggleCameraType = () => {
+    setType(current =>
+      current === CameraType.back ? CameraType.front : CameraType.back
     );
+  };
+
+  return (
+    <View style={{ width:' 90%', height: '10rem' }}>
+      <Camera type={type}>
+        <View>
+          <TouchableOpacity onPress={toggleCameraType}>
+            <Text>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
 };
 
 export default App;
