7e11d91c192ad32c39008d6d1cb67223ac356d62
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-04-10 19:11:07 -0300

    feat(search): implementing the component

---
+++ components/Main/Search.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+const Search = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: center, justifyContent: center }}>
+      <Text>Hello Search</Text>
+    </View>
+  );
+};
+
+export default Search;
